9a40ec727ac71102a80c0bc8a77821f24c75a381
Siddhant <siddhant.prasad8@gmail.com>
2025-04-19 03:34:07 +0530

    feat: Initial setup with basic routing and homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+  return (
+    <main className="home-container">
+      <h1>Welcome to MoodTracker</h1>
+      <p>Start tracking your emotions today</p>
+    </main>
+  );
+};
+
+export default Home;
